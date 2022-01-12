baaf1741b37d5254db6edb660fa9627a2b31e8f8
flaviaresende98 <flaviaresende98@gmail.com>
2022-01-12 16:00:15 -0300

    oficial

--- js/custom.js
+++ js/custom.js
@@ -16,10 +16,9 @@
         });
     });
 
-/* scrollspy  
+/* scrollspy 
 ----------------------------------------------*/
 $('body').scrollspy({ target: '#navbar-scroll' })
-$('body').scrollspy({target: '#container'})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
